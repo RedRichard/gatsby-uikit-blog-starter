fd16e3a8441b6e0bc99ac12a1862f55963966dcf
Ricardo HG <gomric@hotmail.com>
2020-08-13 02:13:16 -0500

    Update: Colección de "Artículos" lista

--- src/components/articles/articleList.js
+++ src/components/articles/articleList.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Card from "../cards/frontPageCard"
 
-const textList = ({ articles }) => {
+const articleList = ({ articles }) => {
   return (
     <div
       className="uk-grid-match uk-grid-column-medium uk-grid-row-medium uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center"
@@ -17,4 +17,4 @@ const textList = ({ articles }) => {
   )
 }
 
-export default textList
+export default articleList
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,20 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
 // Components
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import TextList from "../components/texts/textList"
+import Layout from "components/layout"
+import SEO from "components/seo"
+import TextList from "components/articles/articleList"
 
-import Banner from "../images/main-banner.png"
+import Banner from "images/main-banner.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="uk-section uk-section-default uk-padding-remove-top">
+    {/* <div className="uk-section uk-section-default uk-padding-remove-top">
       <div className="uk-container uk-container-large uk-padding-remove-left uk-padding-remove-right">
         <img src={Banner} alt="Letras Transformadoras"></img>
       </div>
-    </div>
+    </div> */}
     {/* <StaticQuery
       query={graphql`
         query {
