12d5ce8ab7ad1fe88c10c6737ce84020ef390cf2
Ricardo HG <gomric@hotmail.com>
2020-08-18 17:37:48 -0500

    Update: Test data

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,33 +1,39 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import ArticlesComponent from "../components/articles"
+import SEO from "../components/seo"
+// import ArticlesComponent from "../components/articles"
 import Layout from "../components/layout"
 
-export const query = graphql`
-  query Category($url: String!) {
-    articles: allStrapiArticle(filter: { category: { url: { eq: $url } } }) {
-      edges {
-        node {
-          strapiId
-          title
-          category {
-            name
-          }
-          image {
-            publicURL
-          }
-        }
-      }
-    }
-    category: strapiCategory(url: { eq: $url }) {
-      name
-    }
-  }
-`
+// export const query = graphql`
+//   query Category($url: String!) {
+//     articles: allStrapiArticle(filter: { category: { url: { eq: $url } } }) {
+//       edges {
+//         node {
+//           strapiId
+//           title
+//           category {
+//             name
+//           }
+//           image {
+//             publicURL
+//           }
+//         }
+//       }
+//     }
+//     category: strapiCategory(url: { eq: $url }) {
+//       name
+//     }
+//   }
+// `
 
 const Category = () => {
-  return <div>Hola</div>
+  return (
+    <Layout>
+      <SEO title="category" />
+      <div>Hola</div>
+    </Layout>
+  )
 }
 
 export default Category
